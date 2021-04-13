d63885afe28c3d4ef4a8e253dde3b4b6edc892ae
Ajaykumar939 <eppakayalaajaykumar@gmail.com>
2021-04-13 12:28:30 +0530

    apr13-python

---
+++ Helloworld.py
@@ -0,0 +1,1 @@
+print("Hai, Happy UGADI")
---
+++ comment.py
@@ -0,0 +1,13 @@
+print('-----single-line comment------shift + #')
+#print('1')
+print('2')
+print('3')
+print('4')
+print('5')
+
+print('-----multi-line comment------shift + #')
+'''print('1')
+print('2')
+print('3')
+print('4')'''
+print('5')
