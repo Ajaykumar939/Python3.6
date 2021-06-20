fed42dcf1263da7a0bd235a889b34ea147e23635
Ajaykumar939 <eppakayalaajaykumar@gmail.com>
2021-06-20 12:12:54 +0530

    OOps

---
+++ Task_1/Task2.py
@@ -0,0 +1,21 @@
+def ajay():
+    print('haii')
+
+ajay()
+
+
+for i in "venkatesh":
+    print(i)
+
+for i in range(1,100,2):
+    print(i)
+
+x=[1,2,3,4,5,6,7,8]
+print(x)
+
+for i in x:
+    print(i)
+
+ajay=[1,2,3]
+print(ajay)
+
---
+++ Task_1/task.py
@@ -0,0 +1,95 @@
+#expale for list
+x=[1,2,3]
+print(x)
+
+
+#exampl for tupl
+x=(1,2,3)
+print(x)
+
+#exapl for Dictionary
+Dict={1:'ajay', 2:'vijay', 3:'anil'}
+print(Dict)
+
+#eaxmple for set
+x={7,8,9}
+print(x)
+
+#example for add elment in list
+x=[1, "hello", 3.4]
+x.insert(1, 'ajay')
+print(x)
+
+x=[1,2,3]
+z = ['a','b','c']
+
+y = [6,7,8]
+z.extend(y)
+print(z)
+
+
+
+
+x=[1,2,3,4,1,2,3]
+print(x)
+
+y=(1,2,3,4,1,2,3)
+print(y)
+
+x1 = {1,2,3,4,1,2,3}
+print(x1)
+
+x2={1:'ajay',2:'ajay',3:'ajay'}
+print(x2)
+
+#eaxmple for if statments
+if True:
+    print("ajay")
+
+if False:
+    print('vijay')
+
+a=20
+if a<19:
+    print("anil")
+
+b=22
+if b>10:
+    print("mage")
+
+#example for class & mothod
+class carona:
+    def ajay(self):
+        print('i am positive...')
+    def vijay(self):
+        print('i am nagative...')
+
+result=carona()
+result.ajay()
+# result.vijay()
+
+
+#Example for Functons
+def nithin():
+    print('i am nagative...')
+
+def shekar():
+    print('i am positive...')
+
+nithin()
+
+
+def green():
+    print('very good...')
+
+def yello():
+    print('very bad...')
+
+green()
+
+#example for FOR LOOP
+for i in range(1,6):
+    print(i)
+
+for i in range(1,1000):
+    print(i)
